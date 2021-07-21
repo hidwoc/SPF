7280b37e4648bf63607371e910a2381610ea6758
hidwoc <heidischoi@gmail.com>
2021-07-21 16:14:54 -0400

    Create Footer Component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        &copy Salman Ansari, Heidi Choi, Trisha Marchena 2021
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
