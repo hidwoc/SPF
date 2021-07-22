3bd48f6283f33bf3a649a49cdc733470d8646620
trishamarchena <galadrielvasquez@gmail.com>
2021-07-22 09:29:49 -0400

    removed view from card and added Layout to the sunscreens page

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,14 +1,15 @@
 const Card = (props) => {
   return (
-      <div className="sunscreen-card">
-
-              <img className="sunscreen-card-image" src={props.imgURL} alt={props.name} />
-              <div>View</div>
+    <div className="sunscreen-card">
+      <img
+        className="sunscreen-card-image"
+        src={props.imgURL}
+        alt={props.name}
+      />
       <div className="sunscreen-name">{props.name}</div>
       <div className="price">{`$${props.price}`}</div>
-         
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Card
+export default Card;
--- client/src/screens/Sunscreens/Sunscreens.jsx
+++ client/src/screens/Sunscreens/Sunscreens.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { getAllSunscreens } from "../../services/sunscreens";
 import { Link } from "react-router-dom";
 import Card from "../../components/Card/Card";
+import Layout from "../../components/Layout/Layout";
 
 const Sunscreens = (props) => {
   const [sunscreens, setSunscreens] = useState([]);
@@ -15,21 +16,27 @@ const Sunscreens = (props) => {
   }, []);
 
   return (
-    <main>
-      <div className="sunscreeens">
-        {sunscreens.map((sunscreen) =>(
-          <Link className="card" to={`/sunscreens/${sunscreen._id}`}>
-            <Card
-              _id={sunscreen._id}
-              name={sunscreen.name}
-              imgURL={sunscreen.imgURL}
-              price={sunscreen.price}
+    <Layout>
+      <main>
+        <div className="allTheScreens">
+          {sunscreens.map((sunscreen) => (
+            <Link
+              className="cardLink"
+              to={`/sunscreens/${sunscreen._id}`}
               key={sunscreen._id}
-            />
-          </Link>
-        ))}
-      </div>
-    </main>
+            >
+              <Card
+                _id={sunscreen._id}
+                name={sunscreen.name}
+                imgURL={sunscreen.imgURL}
+                price={sunscreen.price}
+                key={sunscreen._id}
+              />
+            </Link>
+          ))}
+        </div>
+      </main>
+    </Layout>
   );
 };
 
