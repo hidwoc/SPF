4e5f0f5d5f8b0e7c7df2356271b56ad81b037122
hidwoc <heidischoi@gmail.com>
2021-07-22 09:59:22 -0400

    Pass user to Navbar as props

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from "../Navbar/Navbar";
 const Layout = (props) => {
   return (
     <div className="layout">
-      <Navbar />
+      <Navbar user={props.user}/>
       <div className="layout-children">
         {props.children}
       </div>
